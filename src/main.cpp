0d192f7f348500d98d87d10cb7df3cc4a6c7404a
D4rkest <elster2005@gmail.com>
2022-05-07 19:21:00 +0300

    before implementing web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,62 @@
 #include <Arduino.h>
 #include "M5Atom.h"
+#include "utility/Power.h"
 #include "Adafruit_SGP30.h"
 //#include "Arduino_JSON.h"
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
+#include "bottoken.cpp"
+#include "SPIFFS.h"
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameter in HTTP POST request
+const char* PARAM_INPUT_1 = "ssid";
+const char* PARAM_INPUT_2 = "pass";
+const char* PARAM_INPUT_3 = "ip";
+const char* PARAM_INPUT_4 = "gateway";
 
 // Wifi network station credentials
 #define WIFI_SSID "ROSSIA BEZ PUTINA_2g"
 #define WIFI_PASSWORD "Tassadar"
-// Telegram BOT Token (Get from Botfather)
-#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 uint8_t nPix = 0;
-CRGB colPix = CRGB(255, 0,0);
+CRGB RED = CRGB(255, 0,0);
+CRGB GREEN = CRGB(0, 255,0);
 Adafruit_SGP30 sgp;
+POWER power;
 
-const long interval = 5000;
+const long sensor_interval = 5000;
+const long bot_interval = 5000;
 unsigned long previousMillis = 0;
+unsigned long previousMillisBot = 0;
 unsigned long currentMillis = 0;
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BOT_TOKEN, secured_client);
 unsigned long bot_lasttime;  
 
-extern char greenRect[2+75];
-extern unsigned char AtomImageData[375 + 2];
+extern char greenRect[];
+
+void connectToWifi(){
+  
+}
 
 void setup() {
   M5.begin(true, false, true); 
+  setCpuFrequencyMhz(80);
+  //esp_sleep_enable_timer_wakeup(min(sensor_interval, bot_interval)*1000);
 
-   if (!sgp.begin()){  //Init the sensor. 初始化传感器
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  if (!sgp.begin()){  //Init the sensor
     Serial.println("Sensor not found");
     while (1);
   }
@@ -43,7 +69,10 @@ void setup() {
   Serial.print("Connecting to Wifi SSID ");
   Serial.print(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  WiFi.softAPIP
+  // Add root certificate for api.telegram.org
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  // secured_client.setInsecure();
   uint8_t n = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -55,11 +84,33 @@ void setup() {
   }
   Serial.print("\nWiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
+  bool wifiSleepEn = WiFi.setSleep(true);
+  Serial.printf("WiFi sleep was enabled: %s\r\n", wifiSleepEn ? "true" : "false");
+  //configTime(0, 0, "pool.ntp.org");  
+}
+
+void handleNewMessages() {
+  M5.dis.drawpix(nPix, GREEN);
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  Serial.printf("new msgs count:%d\n\rlast_i:%ld\n\r", numNewMessages, bot.last_message_received);
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String h2raw = String("rawH2=") + sgp.rawH2 + "\n\r";
+    String ethanolRaw = String("rawEthanol=") + sgp.rawEthanol + "\n\r";
+    String tvoc = String("TVOC=") + sgp.TVOC + "\n\r";
+    String eco2 = String("eCO2=") + sgp.eCO2 + "\n\r";
+    String response = h2raw + ethanolRaw + tvoc + eco2;
+
+    bot.sendMessage(bot.messages[i].chat_id, response);
+  }
+  M5.dis.drawpix(nPix, RED);
 }
 
 void loop() {
+  M5.dis.clear();
+  M5.dis.drawpix(nPix, RED);
   currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= sensor_interval) {
         previousMillis = currentMillis;
 
         if (!sgp.IAQmeasure()) {
@@ -76,11 +127,15 @@ void loop() {
         Serial.printf("rawEthanol=%d\n\r", sgp.rawEthanol);
         Serial.printf("TVOC=%d\n\r", sgp.TVOC);
         Serial.printf("eCO2=%d\n\r", sgp.eCO2);
-
+        }
+        
+  if (currentMillis - previousMillisBot >= bot_interval) {
+    previousMillisBot = currentMillis;
+    handleNewMessages();
   }
-
-  M5.dis.clear();
-  M5.dis.drawpix(nPix, colPix);
+      
   nPix = (nPix + 1) % 25; 
+  Serial.flush();
+  //esp_err_t err = esp_light_sleep_start();
   delay(200);
 }
